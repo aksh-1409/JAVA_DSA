b372dba5b0709ed8b2a7eb7796b5676d236856a3
Aksh Usrathe <akshusrathe007@gmail.com>
2025-06-02 00:09:51 +0530

    Day_13_Find_Pairs_Sum

--- Day_13_Find_Pairs_Sum.java
+++ Day_13_Find_Pairs_Sum.java
@@ -5,24 +5,31 @@ class Day_13_Find_Pairs_Sum{
     int arr[] = {3, -2, 7, 0, 5, -5, 2, -3, 6, 1};  
 
     int targetSum = 4;
+    
 
    findSumPairs(arr,targetSum);
     }
 
     static void findSumPairs(int [] arr, int target){
-       
+        
         if(arr.length<2){
             System.out.println("Not enough elements to form pairs");
             return;
         }
+        boolean flag = false;
         int sum = target;
         for(int i=0;i<arr.length-1;i++){
             for(int j=i+1;j<arr.length;j++){
                if((arr[i]+arr[j])==sum){
                System.out.print("("+ arr[i] + ", " + arr[j] + ")");
-               System.out.println(); 
+               System.out.println();
+               flag = true; 
                }
             }
+        }
+
+        if(!flag){
+            System.out.println("No Pairs");
         }
 
     } 
